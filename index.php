eb51e716b0f70d5c65320536a01eaad28f341eab
pkrogh <pkrogh>
2007-06-27 17:30:13 +0000

    Copied from main.php

--- index.php
+++ index.php
@@ -1,42 +1,29 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+$pageKeywords	= "";
+$pageAuthor		= "Shaun Smith based on work by Bob Fraser";
 
-	#*****************************************************************************
-	#
-	# template.php
-	#
-	# Author: 		Denis Roy
-	# Date:			2005-06-16
-	#
-	# Description: Type your page comments here - these are not sent to the browser
-	#
-	#
-	#****************************************************************************
-	
-	#
-	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "EclipseLink";
-	$pageKeywords	= "Type, page, keywords, here";
-	$pageAuthor		= "Type your name here";
-	
-	# Add page-specific Nav bars here
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+$root = $_SERVER['DOCUMENT_ROOT'];
+require_once ($root . '/eclipselink/common.php');
 
-	# End: page-specific settings
-	#
-		
-	# Paste your HTML content between the EOHTML markers!	
-	$html = <<<EOHTML
+# Generate the web page
+// Load the XML source
+$xml = DOMDocument::load('main.xml');
 
-	<div id="midcolumn">
-	  This project was just provisioned, so there is nothing to see here.
-	</div>
+//Set the page title
+$xpath = new DOMXPath($xml);
+$titleNode = $xpath->query("/sections/attribute::title")->item(0);
+$pageTitle = ($titleNode != null) ? $titleNode->nodeValue : "eclipse.org eclipselink page";
 
+// Load the XSL source
+$xsl = DOMDocument::load($root . '/eclipselink/eclipselinkpage.xsl');
+
+// Configure the transformer
+$proc = new XSLTProcessor;
+$proc->importStyleSheet($xsl); // attach the xsl rules
+
+$maincontent = $proc->transformToXML($xml);
+$html = <<<EOHTML
+$maincontent
 EOHTML;
-
-
-	# Generate the web page
-	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
