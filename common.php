832658118a274a79b3b020911641fcc800965036
dclarke <dclarke>
2007-10-31 21:47:26 +0000

    *** empty log message ***

--- common.php
+++ common.php
@@ -9,6 +9,7 @@
 	function add2users() {
 	   global $Nav;
 		 $Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/EclipseLink", "", 2);
+		 $Nav->addCustomNav("Users Mailing List", "https://dev.eclipse.org/mailman/listinfo/eclipselink-users", "", 2);
 		 $Nav->addCustomNav("User Manual", "http://wiki.eclipse.org/EclipseLink/UserManual", "", 2);
 	}   
 	
