4223a58d0d116144935d8b7a2419e62d35bc36d0
pkrogh <pkrogh>
2007-11-21 15:06:17 +0000

    moving xml to autogenerated one.

--- downloads/index.php
+++ downloads/index.php
@@ -7,7 +7,7 @@ require_once ($root . '/eclipselink/common.php');
 
 # Generate the web page
 // Load the XML source
-$xml = DOMDocument::load('index.xml');
+$xml = DOMDocument::load('http://download.eclipse.org/technology/eclipselink/downloads.xml');
 
 //Set the page title
 $xpath = new DOMXPath($xml);
