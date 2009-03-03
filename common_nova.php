4e90146d434d26c40ebacf120d7ece807c659087
dclarke <dclarke>
2009-03-03 15:43:09 +0000

    *** empty log message ***

--- common_nova.php
+++ common_nova.php
@@ -18,21 +18,22 @@
 	$Nav->addCustomNav("Maven", "http://wiki.eclipse.org/EclipseLink/Maven", "_self", 2);
 	
 	$Nav->addNavSeparator("Community", "/eclipselink/community.php");
+	$Nav->addCustomNav("News", "/eclipselink/news.php", "_self", 2);
 	$Nav->addCustomNav("Newsgroup", "/newsportal/thread.php?group=eclipse.rt.eclipselink", "_self", 2);
 	$Nav->addCustomNav("Users Mailing List", "http://dev.eclipse.org/mhonarc/lists/eclipselink-users/maillist.html", "_self", 2);
+	$Nav->addCustomNav("Team", "/eclipselink/team/", "_self", 2);
 
-	$Nav->addNavSeparator("Documentation", "http://wiki.eclipse.org/EclipseLink/UserManual");
+	$Nav->addNavSeparator("Documentation", "http://wiki.eclipse.org/EclipseLink/UserGuide");
 	$Nav->addCustomNav("FAQ", "http://wiki.eclipse.org/EclipseLink/FAQ", "_self", 2);
 	$Nav->addCustomNav("User Guide", "http://wiki.eclipse.org/EclipseLink/UserManual", "_self", 2);
 	$Nav->addCustomNav("Search User Guide", "http://www.google.com/coop/cse?cx=016171230611334810008%3A8qajdsud9zq&hl=en", "_self", 2);
 	$Nav->addCustomNav("API (JavaDocs)", "/eclipselink/api/latest/index.html", "_self", 2);
 	$Nav->addCustomNav("EclipseLink Wiki", "http://wiki.eclipse.org/EclipseLink", "_self", 2);
 
-	$Nav->addNavSeparator("Project", "http://wiki.eclipse.org/EclipseLink/UserManual");
-	$Nav->addCustomNav("Plan", "http://www.eclipse.org/projects/project-plan.php?projectid=eclipselink", "_self", 2);
-	$Nav->addCustomNav("Timeline", "http://www.eclipse.org/projects/timeline/index.php?projectid=rt.eclipselink", "_self", 2);
-	$Nav->addCustomNav("News", "/eclipselink/news.php", "_self", 2);
-	$Nav->addCustomNav("Team", "/eclipselink/team/", "_self", 2);
+	$Nav->addNavSeparator("Project");
+	$Nav->addCustomNav("Plan", "/projects/project-plan.php?projectid=rt.eclipselink", "_self", 2);
+	$Nav->addCustomNav("Timeline", "/projects/timeline/index.php?projectid=rt.eclipselink", "_self", 2);
+	$Nav->addCustomNav("Releases", "http://wiki.eclipse.org/EclipseLink/Release", "_self", 2);
 
   
 	#*****************************************************************************
