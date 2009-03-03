84c756489468f1d6c6d9b626e9b706ac1fa5e49b
egwin <egwin>
2009-03-03 18:02:31 +0000

    added debug code to header

--- downloads/build-failures.php
+++ downloads/build-failures.php
@@ -1,5 +1,9 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-$pageKeywords	= "bf fb";
+if($_GET['debug']) {
+    ini_set("error_reporing", E_ALL); ini_set("display_errors", "on");
+}
+// This page should remain unlinked and unsearchable
+$pageKeywords	= "eric"; 
 $pageAuthor		= "Eric Gwin based upon work by Doug Clarke (download.php)";
 
 $root = $_SERVER['DOCUMENT_ROOT'];
