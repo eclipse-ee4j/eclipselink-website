0169b5283b72152271a2aa686e73daf8b0fcb922
pkrogh <pkrogh>
2009-03-11 17:54:20 +0000

    1.1

--- common_nova.php
+++ common_nova.php
@@ -22,7 +22,7 @@
 	$Nav->addCustomNav("SDO", "/eclipselink/sdo.php", "_self", 2);
 	$Nav->addCustomNav("DBWS", "/eclipselink/dbws.php", "_self", 2);
   
- 	$Nav->addNavSeparator("Downloads", "/eclispelink/downloads/index.php");
+ 	$Nav->addNavSeparator("Downloads", "/eclipselink/downloads/index.php");
 	$Nav->addCustomNav("1.1.0", "/eclipselink/downloads/index.php#1.1.0", "_self", 2);
 	$Nav->addCustomNav("1.0.2", "/eclipselink/downloads/index.php#1.0.2", "_self", 2);
 	$Nav->addCustomNav("Milestones", "/eclipselink/downloads/milestones.php", "_self", 2);
