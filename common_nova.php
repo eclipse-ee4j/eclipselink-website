7fe69fd479ace85475bca54be71527f0a0d9fa43
dclarke <dclarke>
2009-03-11 15:17:28 +0000

    *** empty log message ***

--- common_nova.php
+++ common_nova.php
@@ -12,7 +12,7 @@
 	$Nav->addCustomNav("DBWS", "/eclipselink/dbws.php", "_self", 2);
   
  	$Nav->addNavSeparator("Downloads", "/eclispelink/downloads/index.php");
-	$Nav->addCustomNav("1.1", "/eclipselink/downloads/index.php#1.1", "_self", 2);
+	$Nav->addCustomNav("1.1", "/eclipselink/downloads/index.php#1.1.0", "_self", 2);
 	$Nav->addCustomNav("1.0.2", "/eclipselink/downloads/index.php#1.0.2", "_self", 2);
 	$Nav->addCustomNav("Milestones", "/eclipselink/downloads/milestones.php", "_self", 2);
 	$Nav->addCustomNav("Maven", "http://wiki.eclipse.org/EclipseLink/Maven", "_self", 2);
