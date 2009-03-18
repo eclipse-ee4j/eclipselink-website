1a927b16641616401052c320675fc15423e586be
egwin <egwin>
2009-03-18 18:11:42 +0000

    fix to fix

--- downloads/index2.php
+++ downloads/index2.php
@@ -1,4 +1,4 @@
-<?php
+<?php                                                                                                                                                                                                                                           require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");   require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");   require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");  $App    = new App();    $Nav    = new Nav();    $Menu   = new Menu();       include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 if($_GET['debug']) {
     ini_set("error_reporing", E_ALL); ini_set("display_errors", "on");
 }
