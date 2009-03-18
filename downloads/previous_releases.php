f05ab0961bd35f2c89c107fc9a7b5ba440b2e252
egwin <egwin>
2009-03-18 20:17:50 +0000

    Put test changes into production.

--- downloads/previous_releases.php
+++ downloads/previous_releases.php
@@ -1,9 +1,9 @@
-<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+<?php                                                                                                                                                                                                                                           require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");   require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");   require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");  $App    = new App();    $Nav    = new Nav();    $Menu   = new Menu();       include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 if($_GET['debug']) {
     ini_set("error_reporing", E_ALL); ini_set("display_errors", "on");
 }
-$pageKeywords	= "Eclipse Persistence EclipseLink Download";
-$pageAuthor		= "Doug Clarke based on work by Bob Fraser";
+$pageKeywords   = "Eclipse Persistence EclipseLink Download";
+$pageAuthor     = "Doug Clarke based on work by Bob Fraser";
 
 $root = $_SERVER['DOCUMENT_ROOT'];
 require_once ($root . '/eclipselink/common_nova.php');
