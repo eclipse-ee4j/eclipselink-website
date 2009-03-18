20a4ee747e05c47ac797fbb93c6171a8e36b2b9d
egwin <egwin>
2009-03-18 15:28:12 +0000

    updated to match Doug's production changes and preview proposed cleanup

--- downloads/index2.php
+++ downloads/index2.php
@@ -1,12 +1,12 @@
-<?php   
+<?php
 if($_GET['debug']) {
     ini_set("error_reporing", E_ALL); ini_set("display_errors", "on");
-}                                                                                                                    require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");   require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");   require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");  $App    = new App();    $Nav    = new Nav();    $Menu   = new Menu();       include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+}
 $pageKeywords   = "Eclipse Persistence EclipseLink Download";
 $pageAuthor     = "Doug Clarke based on work by Bob Fraser";
 
 $root = $_SERVER['DOCUMENT_ROOT'];
-require_once ($root . '/eclipselink/common.php');
+require_once ($root . '/eclipselink/common_nova.php');
 
 # Generate the web page
 // Load the XML source
@@ -18,7 +18,7 @@ $titleNode = $xpath->query("/sections/attribute::title")->item(0);
 $pageTitle = ($titleNode != null) ? $titleNode->nodeValue : "eclipse.org eclipselink download page";
 
 // Load the XSL source
-$xsl = DOMDocument::load($root . '/eclipselink/eclipselinkpage.xsl');
+$xsl = DOMDocument::load($root . '/eclipselink/eclipselink_nova_2_column.xsl');
 
 // Configure the transformer
 $proc = new XSLTProcessor;
