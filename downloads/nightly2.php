ecaea00cbcda24eeb6e959775706553ee0ba7719
egwin <egwin>
2010-10-18 17:16:28 +0000

    update for testing.

--- downloads/nightly2.php
+++ downloads/nightly2.php
@@ -1,13 +1,16 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+if($_GET['debug']) {
+    ini_set("error_reporing", E_ALL); ini_set("display_errors", "on");
+}
 $pageKeywords	= "Eclipse Persistence EclipseLink Download";
 $pageAuthor		= "Doug Clarke based on work by Bob Fraser";
 
 $root = $_SERVER['DOCUMENT_ROOT'];
-require_once ($root . '/eclipselink/common.php');
+require_once ($root . '/eclipselink/common_nova.php');
 
 # Generate the web page
 // Load the XML source
-$xml = DOMDocument::load('http://download.eclipse.org/rt/eclipselink/downloads-new.xml');
+$xml = DOMDocument::load('http://download.eclipse.org/rt/eclipselink/nightly.xml');
 
 //Set the page title
 $xpath = new DOMXPath($xml);
@@ -15,7 +18,7 @@ $titleNode = $xpath->query("/sections/attribute::title")->item(0);
 $pageTitle = ($titleNode != null) ? $titleNode->nodeValue : "eclipse.org eclipselink download page";
 
 // Load the XSL source
-$xsl = DOMDocument::load($root . '/eclipselink/eclipselinkpage.xsl');
+$xsl = DOMDocument::load($root . '/eclipselink/eclipselink_nova_2_column.xsl');
 
 // Configure the transformer
 $proc = new XSLTProcessor;
