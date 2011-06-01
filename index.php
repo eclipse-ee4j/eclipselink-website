09b69d008abfa2317a90a2be5c72dbaacba0c52c
dclarke <dclarke>
2011-06-01 15:43:41 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -24,36 +24,6 @@ $proc->importStyleSheet($xsl); // attach the xsl rules
 $maincontent = $proc->transformToXML($xml);
 $html = <<<EOHTML
 $maincontent
-		<div class="sideitem">
-			<div align="left" id="twtr-search-widget"></div>
-			<script src="http://widgets.twimg.com/j/1/widget.js"></script>
-			<link href="http://widgets.twimg.com/j/1/widget.css" type="text/css" rel="stylesheet">
-			<script>
-				new TWTR.Widget({
-				  search: 'eclipselink',
-				  id: 'twtr-search-widget',
-				  loop: true,
-				  title: 'What\'s being said about...',
-				  subject: 'EclipseLink',
-				  width: 240,
-				  height: 400,
-				  theme: {
-				    shell: {
-				      background: '#002E62',
-				      color: '#ffffff'
-				    },
-				    tweets: {
-				      background: '#ffffff',
-				      color: '#444444',
-				      links: '#002E62'
-				    }
-				  }
-				}).render().start();
-			</script>
-			<br/><font size=-1>Follow: <a href="http://twitter.com/doug_clarke">Doug Clarke</a> (co-lead)
-			<br/>Follow: <a href="http://twitter.com/shaunmsmith">Shaun Smith</a></font>
-		</div>
-
 EOHTML;
 $App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
