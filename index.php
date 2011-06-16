ab379b32c6c130c32000a3c78bea0fc8c3df9675
dclarke <dclarke>
2011-06-16 14:30:05 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -205,6 +205,8 @@ new TWTR.Widget({
 		</div>
 		
 		<div class="sideitem">
+			<a href="/eclipsert/"><img src="/equinox/images/EclipseRT.png" alt="EclipseRT"></a>
+		<br/>
 		<script type="text/javascript" src="http://www.ohloh.net/p/13434/widgets/project_partner_badge.js"></script>
 		</div>
 </div>
