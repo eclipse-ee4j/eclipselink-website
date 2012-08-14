e5d9d036b3c64751e662e98d397b7dbde42da4b2
rsapir <rsapir>
2012-08-14 11:25:41 +0000

    linked to new doc center (HTML)

--- index.php
+++ index.php
@@ -50,9 +50,9 @@ $Nav->addCustomNav( "About This Project",
 			</td>
 			<td>
 				<div class="bigbuttonsDocumentation">
-					<a href="http://wiki.eclipse.org/EclipseLink/Documentation_Center"><div class="bigbuttonsTitleLink"></div></a>
+					<a href="/documentation/index.php"><div class="bigbuttonsTitleLink"></div></a>
 					<span>
-						<a href="http://wiki.eclipse.org/EclipseLink/Documentation_Center">Doc Center</a>,
+						<a href="/documentation/index.php">Doc Center</a>,
 						<a href="/eclipselink/api/index.php"> Java Docs</a>,
 						<a href="http://wiki.eclipse.org/EclipseLink/Examples"> Examples</a>,
 						<a href="http://wiki.eclipse.org/EclipseLink/FAQ"> FAQ</a>, <br/>
