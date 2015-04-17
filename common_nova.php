ceb02f158cae57881836e4aee2b82d553be82b0e
Martin Grebac <martin.grebac@oracle.com>
2015-04-17 20:40:29 +0200

    replace with links to new home

--- common_nova.php
+++ common_nova.php
@@ -16,19 +16,17 @@
 	$Nav->setLinkList(null);
 	
 	$Nav->addNavSeparator("EclipseLink Project", "/eclipselink/index.html");
-	$Nav->addCustomNav("Plan", "/projects/project-plan.php?projectid=rt.eclipselink", "_self", 2);
+	$Nav->addCustomNav("Home", "/eclipselink/index.html", "_self", 2);
 
- 	$Nav->addNavSeparator("Downloads", "/eclipselink/downloads/index.php");
-	$Nav->addCustomNav("2.6.0", "/eclipselink/downloads/index.php#2.4", "_self", 2);
-	$Nav->addCustomNav("Previous", "/eclipselink/downloads/previous_releases.php", "_self", 2);
+ 	$Nav->addNavSeparator("Downloads", "/eclipselink/#download");
+	$Nav->addCustomNav("2.6.0", "/eclipselink/#download", "_self", 2);
 
-	$Nav->addNavSeparator("Documentation", "/eclipselink/documentation/");
-	$Nav->addCustomNav("Doc Center", "/eclipselink/documentation/", "_self", 2);
+	$Nav->addNavSeparator("Documentation", "/eclipselink/#documentation");
+	$Nav->addCustomNav("Doc Center", "/eclipselink/#documentation", "_self", 2);
 	$Nav->addCustomNav("FAQ", "http://wiki.eclipse.org/EclipseLink/FAQ", "_self", 2);
 	$Nav->addCustomNav("API (JavaDocs)", "/eclipselink/api/index.php", "_self", 2);
 	$Nav->addCustomNav("EclipseLink Wiki", "http://wiki.eclipse.org/EclipseLink", "_self", 2);
 
-	$Nav->addNavSeparator("Community", "/eclipselink/community.php");
 	$Nav->addCustomNav("User Forum", "/newsportal/thread.php?group=eclipse.rt.eclipselink", "_self", 2);
 	$Nav->addCustomNav("Users Mailing List", "http://dev.eclipse.org/mhonarc/lists/eclipselink-users/maillist.html", "_self", 2);
 
